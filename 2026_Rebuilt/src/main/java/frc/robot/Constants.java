f9ea990240b6b5639d956aa2453a2ec813b01357
Yao Pan <yaopan0256@gmail.com>
2026-02-28 14:23:14 -0600

    Moved over stuff from Yao-shooter

--- 2026_Rebuilt/src/main/java/frc/robot/Constants.java
+++ 2026_Rebuilt/src/main/java/frc/robot/Constants.java
@@ -4,13 +4,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 
 /**
@@ -28,22 +21,8 @@ public final class Constants {
     public static final int kDriverControllerPort = 0;
     public static final int kFunctionsControllerPort = 1;
 
-
-    //Shooter Motors 
-    public static final int KFlywheelMotor_1 = 15;
-    public static final int KFlywheelMotor_2 = 16;
-    public static final int KBackRollerMotor = 10;
-    
-
     //Intake
-    public static final int KIntakeMotor = 11;
-    public static final int kPivotMotor = 12;
-
-    //Transfer 
-    public static final int kTransferMotor_1 = 13;
-    public static final int kTransferMotor_2 = 14;
-
-    
+    public static final int KIntakeMotor = 10;
 
     //Drive Motors
     public static final int kFrontLeftDrive = 5;
@@ -57,20 +36,7 @@ public final class Constants {
     public static final int kFrontRightSteering = 2;
     public static final int kBackLeftSteering  = 8;
     public static final int kBackRightSteering = 6;
-
-<<<<<<< Updated upstream
-=======
-    //Shooter Motor
-    public static final double flywheelCircumference = 0.3191858136;
-    public static final double backwheelCircumference = 0.0797964534;
-    public static final double shooterAngle = 42; //What's the meaning of life?
-
-    //Shooter Math
-    public static final double hoopHeight = 1.8288; //Later, adjust this a bit so we don't keep landing rim shots (in theory)
-    public static final double gravity = 9.81;
-    public static final double gravity_kansas = 9.806; //adjusted for KANSAS SEA LEVEL BECAUSE I AM A TRYHARD
     
->>>>>>> Stashed changes
     //Encoders
     public static final int kFrontLeftEncoder = 1;
     public static final int kFrontRightEncoder = 0;
@@ -85,7 +51,7 @@ public final class Constants {
 
 
     //Dampners 
-    public static final double kSwerveDampner = 0.05;
+    public static final double kSwerveDampner = 0.50;
     public static final double kElevatorDampner = 0.5;
     public static final double kClimbDampner = 0.5;
     public static final double kAlgaeDampner = 0.2;
@@ -94,21 +60,5 @@ public final class Constants {
     //Chasis Length
     public static final double chasisWidth = Units.inchesToMeters(20);
     public static final double chasisLength = Units.inchesToMeters(24);
-
-
-
-
-    //Camera
-    public static final AprilTagFields kField = AprilTagFields.kDefaultField;
-    public static final String kCameraName = "NAME";
-    public static final Transform3d kRobotToCam = new Transform3d(
-        Units.inchesToMeters(10), 0, Units.inchesToMeters(10), 
-        new Rotation3d(0   , 0, 0)
-    ); 
-
-
-    //Standard Deviations
-    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
-    public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
 
 }
--- 2026_Rebuilt/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ 2026_Rebuilt/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -14,75 +14,101 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
  * Shoots balls currently loaded in the machine.
- * @version v1.3.1
+ * @version v2.0.0
  */
-public class ShooterSubsystem extends SubsystemBase{
+public class ShooterSubsystem extends SubsystemBase
+{
+    // logga
     private static final Logger logger = Logger.getLogger("ShooterSubsystem");
-    
-    private static final double MOTOR_RPM_MULT = Constants.shoooterPowerConversion;
-    private static final double GRAVITY = Constants.gravity_kansas;
-    private static final double GOAL_HEIGHT = Constants.hoopHeight;
-    private static final double ANGLE = Constants.shooterAngle;
-    private SparkMax flywheel = new SparkMax(0, MotorType.kBrushless);
+    // Math-related constants
+    private static final double FLYWHEEL_CIRCUMFERENCE = 0.3191858136;
+    private static final double BACKWHEEL_CIRCUMFERENCE = 0.0797964534;
+    private static final double GRAVITY = 9.806;
+    private static final double GOAL_HEIGHT = 1.8288;
+    private static final double ANGLE = 42;
+
+    // Motors
+    private SparkMax flywheel = new SparkMax(0, MotorType.kBrushless); //TODO: do PID tuning and get the CAN ID's
     private SparkMax backRollers = new SparkMax(0, MotorType.kBrushless);
     private SparkMax feedRollers = new SparkMax(0, MotorType.kBrushless);
 
+
+    // getter land
     /**
-     * Shoots at default power level of 50%.
-     * @since v0.0.0
-     * @version v1.0.0
+     * Checks if the 
+     * @return if error between target RPM and actual RPM is below 3%, return true. otherwise return false
      */
-    public void shoot(){
-        flywheel.set(0.5);
-        backRollers.set(0.5);
-        setFeedRollers();
+    public boolean isReadyToFire()
+    {
+        return true; //TODO: add an encoder and complete this method
     }
-    
+
     /**
-     * Shoots balls at manually set power level.
-     * @param powerLevel The power level to set the flywheels to, range 0.0 to -1.0.
-     * @since v1.0.0
-     * @version v1.3.1
-     */
-    public void shoot(double powerLevel){
-        // Create power level variable that we can actually change, since java no like when parameter changed
-        double power = powerlevel;
-        
-        if(powerLevel > 0){
-            logger.info("Power level exceeds maximum output, set to -1.0");
-            power = -1;
-        }
-        if(powerLevel < 0){
-            logger.warning("for some reason you passed a positive value so i set the power to 0 since i dont want it to get jammed");
-            power = 0;
-        }
-        
-        flywheel.set(powerLevel);
-        backRollers.set(powerLevel);
-        setFeedRollers();
-    }
-    
-    /**
-     * Shoots balls at power level dependent on distance, [insert maximum shooting distance here].
+     * Sets the rollers to spin to make it into a goal at the target distance.
      * @param distance The distance from the thing you're trying to shoot.
      * @since v1.0.0
-     * @version v1.3.0
+     * @version v2.0.0
      */
-    public void shootAtDistance(double distance){ 
+    public void setDistance(double distance)
+    { 
 
-        if(distance < GOAL_HEIGHT+1){
-            logger.info("Too close to goal to fire!! (Minimum distance is 2.83 meters)");
+        if(distance < GOAL_HEIGHT+0.5){
+            logger.info("Too close to goal to fire!! (Minimum distance is about 2.5 meters)");
             return;
         }
         // Finds the velocity the ball needs to travel in order to make it in the goal. (TW: Math...)
         double velocity = Math.sqrt((GRAVITY*Math.pow(distance,2)) / (2 * Math.pow(Math.cos(Math.toRadians(ANGLE)),2) * (distance*Math.tan(Math.toRadians(ANGLE)) - GOAL_HEIGHT)));
-        // Converts velocity to target RPM (takes wheels into consideration)
-        double rpm = velocity*MOTOR_RPM_MULT; //motor needs to rotate X number of times so that in 1 second it spins [velocity] meters
-        // TODO: add a fancy log here telling you about the maximum distance if you exceed it, and then abort firing
+        
+        rev(velocity);
+    }
+
+    /**
+     * Revs up the flywheel and backroller to fire at a set velocity.
+     * @param velocity The velocity in m/s to fire the ball at. 
+     * @since v2.0.0
+     * @version v2.0.0
+     */
+    public void rev(double velocity)
+    {
+        // Converts velocity to target RPM
+        double flywheelRPM = -(velocity/FLYWHEEL_CIRCUMFERENCE)*60;
+        double backwheelRPM = -(velocity/BACKWHEEL_CIRCUMFERENCE)*60;
         
         // Makes the motors spin at the RPM calculated 
-        flywheel.setReference(rpm,ControlType.kVelocity);
-        backRollers.setReference(rpm,ControlType.kVelocity);
+        flywheel.setReference(flywheelRPM,ControlType.kVelocity);
+        backRollers.setReference(backwheelRPM,ControlType.kVelocity);
+    }
+
+    /**
+     * FIRE!!!! (make sure to rev up the flywheels first)
+     * @since v2.0.0
+     * @version v2.0.0
+     */
+    public void shoot()
+    {
+        feedRollers.setReference(120,ControlType.kVelocity);
+    }
+
+    /**
+     * ok now stop shooting
+     * @since v2.0.0
+     * @version v2.0.0
+     */
+    public void stopShooting()
+    {
+        feedRollers.setReference(0,ControlType.kVelocity);
+    }
+
+    // It's so cold here in hell, where all deprecated methods go when they die 
+    /**
+     * Shoots at default power level of 50% voltage. Good for testing purposes
+     * @since v0.0.0
+     * @version v1.3.1
+     */
+    public void testShot()
+    {
+        flywheel.set(-0.5);
+        backRollers.set(-0.5);
         setFeedRollers();
     }
 
@@ -90,8 +116,10 @@ public class ShooterSubsystem extends SubsystemBase{
      * Sets feedRollers to 0 if not firing so that it doesn't get jammed (will it get jammed otherwise?? idk, probably).
      * @since v1.0.0
      * @version v1.0.0
+     * @deprecated
      */
-    public void setFeedRollers(){
+    public void setFeedRollers()
+    {
         if(flywheel.get() < 0.05 || backRollers.get() < 0.05)
             feedRollers.set(0);
         else
